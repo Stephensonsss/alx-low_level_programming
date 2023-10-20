0c1e441ef57b6e139b2400254f6545e1586d0dcf
stephensonsss <stephensonsss3@gmail.com>
2023-10-20 03:09:25 -0700

    task_0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * beforemain - function that executes before main
+ *
+ * Return: void
+ */
+
+/*void beforemain(void)__attribute__ ((constructor));*/
+
+void beforemain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This ia a readme file for 0x12-singly linked lists
